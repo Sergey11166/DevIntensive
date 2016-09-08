ad71a3b6e3881a2c107d3b6a49aa0de46d1358d0
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-09-08 12:12:41 +0600

    Add logs to life-circles methods, add Constants.java

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -2,14 +2,61 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.Constants;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = Constants.LOG_TAG_PREFIX + "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
     }
 }
---
+++ app/src/main/java/com/softdesign/devintensive/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Interface that contains constants of this app
+ *
+ * @author Sergey Vorobyev
+ */
+
+public interface Constants {
+
+    String LOG_TAG_PREFIX = "DEV";
+}
